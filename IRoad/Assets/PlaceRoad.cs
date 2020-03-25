6a9fa89e13bf9015b07a1fde37eff30e1b2c6241
Gino <ginovandegraaf@hotmail.com>
2020-03-25 16:39:46 +0100

    Road Workshop

---
+++ IRoad/Assets/PlaceRoad.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceRoad : MonoBehaviour
+{
+    [SerializeField]
+    GameObject FirstObj;
+    [SerializeField]
+    GameObject PlaceRoadPrefab;
+    Vector3 LastObjectPos;
+    [SerializeField]
+    Vector3 NewPos;
+    const int roadAmount= 100;
+
+    List<GameObject> roadObj = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        roadObj.Add(FirstObj);
+        LastObjectPos = FirstObj.transform.position;
+        StartCoroutine(BuildRoad());
+    }
+    private void Update()
+    {
+        DeleteFirstRoadObj();
+    }
+
+    void DeleteFirstRoadObj()
+    {
+        if(roadObj.Count>=roadAmount)
+        {
+            GameObject road = roadObj[0];
+            roadObj.RemoveAt(0);
+            Destroy(road);
+        }
+    }
+
+    IEnumerator BuildRoad()
+    {
+        Vector3 setNewPos = new Vector3(LastObjectPos.x + NewPos.x, LastObjectPos.y + NewPos.y, LastObjectPos.z + NewPos.z);
+        GameObject road = Instantiate(PlaceRoadPrefab) as GameObject;
+        road.transform.position = setNewPos;
+        LastObjectPos = road.transform.position;
+        roadObj.Add(road);
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(BuildRoad());
+    }
+}
---
+++ IRoad/Assets/camFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camFollow : MonoBehaviour
+{
+    [SerializeField]
+    private float followSpeed = 100f;
+    public Transform target;
+
+    void Update()
+    {
+        Vector3 newPosition = target.position;
+        newPosition.z -=3 ;
+        newPosition.y += 3;
+        transform.position = Vector3.Slerp(transform.position, newPosition, followSpeed * Time.deltaTime);
+    }
+}
